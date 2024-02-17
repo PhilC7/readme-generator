5e996733edfd9f4273092fcc12e7f9700bf51518
Phil Cooper <philjcooper@outlook.com>
2024-02-17 14:08:25 +0000

    added writeto file and init functionality

--- index.js
+++ index.js
@@ -5,17 +5,72 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please give a description of your project."
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "Add your installation details."
+    },
+    {
+        type: "input",
+        name: "useage",
+        message: "Add your useage details."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "Choose a license",
+        choices: ["MIT", "Apache", "Boost 1.0", "EPL 1.0", "EPL 2.0", "None"],
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "Please add information to contribute to the project."
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please add your testing details.",
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Please enter your questions details."
+    },
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
+    // write file method - check for error, if error log error / else log README is ready.
+    fs.writeFile(fileName, data, (err) => {
+        err ? console.error(err) : console.log(`Your README is ready!`);
+    });
 }
 
 // function to initialize program
 function init() {
-
+    inquirer.prompt(questions).then((answer) => {
+        const content = generateMarkdown(answer); // set variable to hold answers
+        writeToFile(generated_README.md, content);
+    });
 }
+
 
 // function call to initialize program
 init();
